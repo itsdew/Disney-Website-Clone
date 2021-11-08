3b55e1b4738cb80b7ddfd16b8d7b51e489dc6ccb
Devendra Uraon <devendra123world@gmail.com>
2021-11-09 03:50:45 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="flex bg-gray-900 text-white min-h-screen">
+    <SideBar/>
+    <div class="w-full">
+      <transition name="page" mode="out-in">
+        <router-view/>
+      </transition>
+    </div>
+  </div>
+</template>
+<script>
+  import SideBar from "./views/SideBar";
+  export default {
+    components: { SideBar }
+  }
+</script>
+
+<style src="./css/main.css">
+
+</style>
